36a44288232e7184fa77acb21ddc16bfeb5ae2c2
Aaron Miller <acmiller273@gmail.com>
2016-10-28 15:21:07 -0400

    Added fixed simulation timestep

--- sim/default.py
+++ sim/default.py
@@ -1,13 +1,8 @@
 #! /usr/bin/env morseexec
 
-"""
-Basic MORSE simulation scene for <sim> environment
-
-Feel free to edit this template as you like!
-"""
-
 import math
-from morse.builder import Environment
+from morse.builder import Clock, Environment
+from morse.core.morse_time import TimeStrategies
 from sim.builder.robots import Roomba, Obstacle, Quadcopter
 
 roomba_placement_radius = 1
@@ -37,5 +32,6 @@ robot.add_service('socket')
 
 # set 'fastmode' to True to switch to wireframe mode
 env = Environment('environment.blend', fastmode = False)
+env.set_time_strategy(TimeStrategies.FixedSimulationStep)
 env.set_camera_location([-18.0, -6.7, 10.8])
 env.set_camera_rotation([1.09, 0, -1.14])
