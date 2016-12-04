4eddb64eb9215e0720c83a6c418c56daa150619a
Aaron Miller <acmiller273@gmail.com>
2016-12-04 01:37:57 +0000

    Fixed sensor positioning on updated roombas

--- sim/src/sim/builder/robots/Roomba.py
+++ sim/src/sim/builder/robots/Roomba.py
@@ -7,8 +7,8 @@ from sim.builder.sensors.TopTouchSensor import TopTouchSensor
 BUMP_SENSOR_ANGLE = 0.489957
 NUM_BUMP_SENSORS = 6
 
-ROOMBA_RADIUS = 0.15
-ROOMBA_HEIGHT = 0.1
+ROOMBA_RADIUS = 0.165
+ROOMBA_HEIGHT = 0.065
 
 class Roomba(GroundRobot):
     """
