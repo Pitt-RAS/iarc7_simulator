7cc6e04c6be3a5809c7752da3ca0f16189f1ef42
Aaron Miller <acmiller273@gmail.com>
2017-02-15 02:16:20 +0000

    Fix axes for new definition of OrientationAngles

--- src/simulator_adapter.py
+++ src/simulator_adapter.py
@@ -65,8 +65,8 @@ def control_direction_callback(direction_msg):
     attitude_msg.layout.data_offset = 0
     attitude_msg.data = [
             direction_msg.data.roll,
-            -direction_msg.data.pitch,
-            -direction_msg.data.yaw,
+            direction_msg.data.pitch,
+            direction_msg.data.yaw,
             0.01
         ]
 
