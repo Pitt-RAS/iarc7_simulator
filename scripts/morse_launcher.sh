13f9f9a24f5b45b85d86c97c7333962fc2a2b8aa
Aaron Miller <acmiller273@gmail.com>
2017-03-05 21:53:02 +0000

    Made morse_launcher.sh use lock file to check if already running

--- scripts/morse_launcher.sh
+++ scripts/morse_launcher.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-PROCESS="blender"
-RESULT=`pgrep ${PROCESS}`
-
-if [ "${RESULT:-null}" = null ]; then
+lockdir=/tmp/morse_launcher.sh.lock
+if mkdir $lockdir 2> /dev/null; then
+    trap "rmdir $lockdir" EXIT INT KILL TERM
     cd ../sim
     morse run sim 2> /tmp/morse.log
+else
+    echo "Failed to create lock directory at /tmp/morse_launcher.sh.lock, exiting..."
 fi
