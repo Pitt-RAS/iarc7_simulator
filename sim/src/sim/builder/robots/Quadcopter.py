c346b57166f838561395325252222dd008d1da25
Aaron Miller <acmiller273@gmail.com>
2017-03-07 05:20:50 +0000

    Improved quad tuning, made closer to reality

--- sim/src/sim/builder/robots/Quadcopter.py
+++ sim/src/sim/builder/robots/Quadcopter.py
@@ -24,11 +24,11 @@ class Quadcopter(Robot):
         ###################################
 
         self.motion = RotorcraftAttitude()
-        self.motion.properties(RollPitchPgain = 1000.0,
-                               RollPitchDgain = 400.0,
-                               YawPgain = 3,
-                               YawDgain = 50,
-                               ThrustFactor = 1,
+        self.motion.properties(RollPitchPgain = 5000.0,
+                               RollPitchDgain = 800.0,
+                               YawPgain = 10000.0,
+                               YawDgain = 1000.0,
+                               ThrustFactor = 1.0,
                                YawRateControl = True
                                )
         self.motion.add_stream('ros', topic='/sim/quad_attitude_controller')
@@ -36,10 +36,12 @@ class Quadcopter(Robot):
 
         self.thrust = Thrust()
         self.thrust.properties(NumberOfProps = 4,
-                               PropDiameter = 0.2,
+                               PropDiameter = 0.25,
                                PropPitch = 0.1,
-                               PropMaxSpeed = 8214 / 60.0)
-        self.thrust.add_stream('ros', 'sim.middleware.ros.thrust.ThrustReader', topic='/sim/quad_thrust_controller')
+                               PropMaxSpeed = 12214 / 60.0)
+        self.thrust.add_stream('ros',
+                               'sim.middleware.ros.thrust.ThrustReader',
+                               topic='/sim/quad_thrust_controller')
         self.append(self.thrust)
 
         ###################################
